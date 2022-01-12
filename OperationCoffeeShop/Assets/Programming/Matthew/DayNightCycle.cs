04482741ec0a8ed894fab09aec50aec595d90d97
CornKernel <88573972+CornKerne1@users.noreply.github.com>
2022-01-12 15:29:05 -0800

    More Comments

--- OperationCoffeeShop/Assets/Programming/Matthew/DayNightCycle.cs
+++ OperationCoffeeShop/Assets/Programming/Matthew/DayNightCycle.cs
@@ -1,43 +1,50 @@
-using System;
+using System;//To use the EventHandler you must INCLUDE
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DayNightCycle
 {
-
+    //Events!
     public event EventHandler TimeChanged;
     public GameModeData gMD;
     private DayNightCycle dNC;
     private GameMode gM;
 
-    public DayNightCycle(DayNightCycle dNC, GameMode gameMode, GameModeData gameModeData)
+    //Constructor!
+    public DayNightCycle(DayNightCycle dNC, GameMode gM, GameModeData gMD)
     {
         this.dNC = dNC;
-        this.gM = gameMode;
-        this.gMD = gameModeData;
+        this.gM = gM;
+        this.gMD = gMD;
     }
-
+    //Handles store open timer.
     public void StartTimer()
     {
+        //If store is open.
         if (gMD.isOpen)
         {
+            //Subtracts the amount that passes from the variable.
             gMD.currentOpenTime -= 1 * Time.deltaTime;
+            //If current time is less than or equal to 0.
             if(gMD.currentOpenTime <= 0)
             {
                 UpdateTimeOfDay(gMD.hoursOpen);
+                //Store closes.
                 gMD.isOpen = false;
+                //Timer Resets.
                 gMD.currentOpenTime = gMD.openTimer;
             }
         }
     }
+    //This updates and handles time of day when days have 24 hours..
     public void UpdateTimeOfDay(int time)
     {
-
+        //If the current time is 2 in the morning, we want to display 2 not 26.
         if (gMD.timeOfDay + time > 24) gMD.timeOfDay = (gMD.timeOfDay + time) - 24;
         else gMD.timeOfDay += time;
         if (gMD.timeOfDay > 12) gMD.displayTime = -1 * (gMD.timeOfDay - 12);
-
+        //If TimeChanged Event is not null (isValid?) Invoke Event. 
         TimeChanged?.Invoke(this, EventArgs.Empty);
     }
 }
--- OperationCoffeeShop/Assets/Programming/Matthew/GameMode.cs
+++ OperationCoffeeShop/Assets/Programming/Matthew/GameMode.cs
@@ -14,17 +14,22 @@ public class GameMode : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Sets the reference to this script in the GameModeData instance.
         gMD.gameModeInstance = this;
+        //Creates new DayNightCycle component.
         dNC = new DayNightCycle(dNC, this, gMD);
+        //Resets old timer data.
         gMD.currentOpenTime = gMD.openTimer;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Handles the timer when the store is open.
         dNC.StartTimer();
     }
 
+    //This is the method to call to change the time of day.
     public void UpdateTimeOfDay(int time)
     {
         dNC.UpdateTimeOfDay(time);
