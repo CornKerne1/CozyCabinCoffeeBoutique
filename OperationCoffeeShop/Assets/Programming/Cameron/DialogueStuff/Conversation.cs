ad2faeaee2ad80511918c247cde92f437cf548d5
snail-001 <52124912+snail-001@users.noreply.github.com>
2022-01-20 12:27:55 -0800

    added dialogue stuff
    added a folder, the system scriptable object, a test character, and a conversation scriptable object (currently broken)

---
+++ OperationCoffeeShop/Assets/Programming/Cameron/DialogueStuff/Conversation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public struct Line
+{
+    public Character character;
+    [TextArea(2, 5)]
+    public string text;
+}
+[CreateAssetMenu(fileName = "New Conversation", menuName = "Conversation")]
+public class Conversation : ScriptableObject
+{
+    public Character player;
+    public Character customer;
+    public Line[] lines;
+}
---
+++ OperationCoffeeShop/Assets/Programming/Cameron/DialogueStuff/DialogueSystem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "new Character", menuName = "Character")]
+public class DialogueSystem : ScriptableObject
+{
+
+    //public void StartDialogue()
+    //{
+
+    //}
+    public string CharacterName;
+    //public Sprite portrait;
+}
