eece416ae1e27b6eea5806f33d9771bbd0f9333b
school-034 <>
2022-04-07 12:24:48 -0700

    attempted to add sound
    added sound from london and robert

---
+++ OperationCoffeeShop/Assets/Scenes/Zach/MusicManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip[] soundtrack;
+
+    // Use this for initialization
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (!GetComponent<AudioSource>().playOnAwake)
+        {
+            GetComponent<AudioSource>().clip = soundtrack[Random.Range(0, soundtrack.Length)];
+            GetComponent<AudioSource>().Play();
+        }
+
+
+        if (GetComponent<AudioSource>().playOnAwake)
+        {
+            GetComponent<AudioSource>().clip = soundtrack[Random.Range(0, soundtrack.Length)];
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GetComponent<AudioSource>().isPlaying)
+        {
+            GetComponent<AudioSource>().clip = soundtrack[Random.Range(0, soundtrack.Length)];
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
