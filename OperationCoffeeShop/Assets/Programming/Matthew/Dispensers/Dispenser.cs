04c44dd54fe477eb9ad9eec5548ef3f6d916bba6
Connor Hartzog <hartzogcr@gmail.com>
2022-05-23 21:34:10 -0700

    customer satisfaction visualized

--- OperationCoffeeShop/Assets/Programming/Matthew/Dispensers/Dispenser.cs
+++ OperationCoffeeShop/Assets/Programming/Matthew/Dispensers/Dispenser.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Dispenser : Interactable
 {
     [SerializeField] private Transform spawnTrans;
     [SerializeField] private ObjectHolder objType;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private string message = " beans remaing";
+
+
     private Transform obj;
 
     public int quantity = 10;
@@ -15,18 +20,34 @@ public class Dispenser : Interactable
     // Start is called before the first frame update
     void Start()
     {
+        if (!bottomless)
+            try
+            {
+                text = GetComponentInChildren<TextMeshProUGUI>();
+                if (!bottomless)
+                    text.text = quantity + message;
+                else text.text = "Botomless";
+                
+            }
+            catch
+            {
+                text = null;
+            }
+ 
+
     }
 
     private void Update()
     {
-        
+
     }
 
     public override void OnInteract(PlayerInteraction pI)
     {
-        if (!pI.pD.busyHands && (bottomless || quantity > 0 ))
+        if (!pI.pD.busyHands && (bottomless || quantity > 0))
         {
             quantity--;
+            updateQuantity();
             obj = Instantiate(objType.gObj, spawnTrans.position, spawnTrans.rotation).transform;
             PhysicalIngredient phyIng;
             IngredientContainer ingCon;
@@ -40,6 +61,14 @@ public class Dispenser : Interactable
                 ingCon.inHand = true;
             }
             pI.Carry(obj.gameObject);
+        }
+    }
+
+    private void updateQuantity()
+    {
+        if (text != null)
+        {
+            text.text = quantity + message;
         }
     }
     public override void OnFocus()
