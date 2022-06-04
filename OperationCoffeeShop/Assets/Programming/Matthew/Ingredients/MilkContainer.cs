0dc73025ad0a6e4cc9f3dca883707395c8a738c9
Connor Hartzog <hartzogcr@gmail.com>
2022-06-03 19:03:23 -0700

    shop is better but not perfect

--- OperationCoffeeShop/Assets/Programming/Matthew/Ingredients/MilkContainer.cs
+++ OperationCoffeeShop/Assets/Programming/Matthew/Ingredients/MilkContainer.cs
@@ -1,37 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class MilkContainer : MonoBehaviour
 {
     private int count;
+    IngredientContainer iC;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(Init());
+        ComputerShop.DepositItems += AddItems;
+        iC = GetComponent<IngredientContainer>();
     }
 
     private IEnumerator Init()
     {
         yield return new WaitForSeconds(0.04f);
-        StartCoroutine(AddIngredients());
+        StartCoroutine(AddIngredients(500));
     }
-    
-    private IEnumerator AddIngredients()
+
+    private IEnumerator AddIngredients(int quantity)
     {
         count = count + 1;
         yield return new WaitForSeconds(0.04f);
         var iN = new IngredientNode(Ingredients.Milk, .1f);
-        GetComponent<IngredientContainer>().AddToContainer(iN);
-        if (count < 500)
+        iC.AddToContainer(iN);
+        if (count < quantity)
         {
-            StartCoroutine(AddIngredients());
+            StartCoroutine(AddIngredients(quantity));
         }
     }
+    public void AddItems(object sender, EventArgs e)
+    {
+        try
+        {
+            Tuple<Ingredients, int> tuple = (Tuple<Ingredients, int>)sender;
 
+            if (Ingredients.Milk == tuple.Item1)
+            {
+                this.count += tuple.Item2;
+                StartCoroutine(AddIngredients(count + tuple.Item2*10));
+            }
+        }
+        catch
+        {
+
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
