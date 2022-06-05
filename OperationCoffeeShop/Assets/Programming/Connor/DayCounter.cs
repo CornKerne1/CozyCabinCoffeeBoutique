3f7e7d0842675b6c0c41e5a71a482c1353b568ef
Connor Hartzog <hartzogcr@gmail.com>
2022-06-04 23:37:36 -0700

    day counter

---
+++ OperationCoffeeShop/Assets/Programming/Connor/DayCounter.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DayCounter : MonoBehaviour
+{
+
+    public Image dayDisplay;
+
+    public Sprite day1;
+    public Sprite day2;
+    public Sprite day3;
+
+    GameMode gM;
+
+    private void Start()
+    {
+        gM = GameObject.FindGameObjectWithTag("GameMode").GetComponent<GameMode>();
+
+    }
+
+
+    public void DisplayDay(int day)
+    {
+
+        switch (day)
+        {
+            case 1:
+                dayDisplay.sprite = day1;
+                break;
+            case 2:
+                dayDisplay.sprite = day2;
+                break;
+
+            case 3:
+                dayDisplay.sprite = day3;
+                break;
+
+            default:
+
+                break;
+        }
+        dayDisplay.enabled = true;
+    }
+
+    public void HideDisplay()
+    {
+        dayDisplay.enabled = false;
+    }
+
+}
