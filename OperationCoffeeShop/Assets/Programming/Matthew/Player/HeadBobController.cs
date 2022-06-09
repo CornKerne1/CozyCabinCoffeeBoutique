65843fe8fcef9e34bd12f7d5524e9e0ede62845e
CornKernel <88573972+CornKerne1@users.noreply.github.com>
2022-06-08 23:15:48 -0700

    HeadBob Controller Refactoring

--- OperationCoffeeShop/Assets/Programming/Matthew/Player/HeadBobController.cs
+++ OperationCoffeeShop/Assets/Programming/Matthew/Player/HeadBobController.cs
@@ -6,37 +6,23 @@ using UnityEngine;
 public class HeadBobController : MonoBehaviour
 {
     [SerializeField] private PlayerData pD;
-
     [SerializeField] private bool enable = true;
-
     [SerializeField] private Transform cam;
     [SerializeField] private Transform holder;
-
-    
-    private Vector3 startPos;
-    private Vector3 lastPosition;
-    private CharacterController controller;
-
-
-
-    void Awake()
+    private Vector3 _startPos;
+    private Vector3 _lastPosition;
+    private CharacterController _controller;
+    private void Awake()
     {
-        controller = GetComponent<CharacterController>();
-        startPos = cam.localPosition;
-        lastPosition = transform.root.position;
+        _controller = GetComponent<CharacterController>();
+        _startPos = cam.localPosition;
+        _lastPosition = transform.root.position;
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         CheckMotion();
         ResetPosition();
         cam.LookAt(FocusTarget());
-    }
-
-    private void PlayMotion(Vector3 motion)
-    {
-        cam.localPosition += motion * Time.deltaTime;
     }
 
     private void CheckMotion()
@@ -45,30 +31,34 @@ public class HeadBobController : MonoBehaviour
 
         PlayMotion(Motion());
     }
-
+    private void ResetPosition()
+    {
+        if (cam.localPosition == _startPos) return;
+        cam.localPosition = Vector3.Lerp(cam.localPosition, _startPos, 8 * Time.deltaTime);
+    } 
+    private void PlayMotion(Vector3 motion)
+    {
+        cam.localPosition += motion * Time.deltaTime;
+    }
+    private Vector3 FocusTarget()
+    {
+        var currentPosition = transform.position;
+        var pos = new Vector3(currentPosition.x, currentPosition.y + holder.localPosition.y, currentPosition.z);
+        pos += holder.forward * 15.0f;
+        return pos;
+    }
     private float GetSpeed()
     {
-        float speed = Vector3.Distance(lastPosition, transform.root.position);
-        lastPosition = transform.root.position;
+        var currentRootTrans = transform.root.position;
+        var speed = Vector3.Distance(_lastPosition, currentRootTrans);
+        _lastPosition = currentRootTrans;
         return speed;
     }
     private Vector3 Motion()
     {
-        Vector3 pos = Vector3.zero;
+        var pos = Vector3.zero;
         pos.y += Mathf.Sin(Time.time * pD.frequency) * pD.amplitude;
         pos.x += Mathf.Cos(Time.time * pD.frequency / 2) * pD.amplitude * 2;
         return pos;
     }
-    private Vector3 FocusTarget()
-    {
-        Vector3 pos = new Vector3(transform.position.x, transform.position.y + holder.localPosition.y, transform.position.z);
-        pos += holder.forward * 15.0f;
-        return pos;
-    }
-    
-    private void ResetPosition()
-    {
-        if (cam.localPosition == startPos) return;
-        cam.localPosition = Vector3.Lerp(cam.localPosition, startPos, 8 * Time.deltaTime);
-    }    
 }
