4537517ce176e5d6f140bff2c7b470ef79589919
Connor Hartzog <hartzogcr@gmail.com>
2022-06-10 18:04:42 -0700

    sugar cube fixes

--- OperationCoffeeShop/Assets/Programming/Matthew/Ingredients/SugarCube.cs
+++ OperationCoffeeShop/Assets/Programming/Matthew/Ingredients/SugarCube.cs
@@ -6,38 +6,40 @@ using Random = UnityEngine.Random;
 
 public class SugarCube : MonoBehaviour
 {
-    [SerializeField]private IngredientNode iN;
+    [SerializeField] private IngredientNode iN;
     [SerializeField] private GameObject sugarCube;
     private IEnumerator _coRef;
-    
 
     private void OnTriggerEnter(Collider other)
     {
         TryAddOrDelete(other.gameObject);
-
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (_coRef != null) return;
-        _coRef = MakePickUp();
-        StartCoroutine(MakePickUp());
+        _coRef = CO_MakePickUp();
+        StartCoroutine(CO_MakePickUp());
     }
 
-    private IEnumerator MakePickUp()
+    private IEnumerator CO_MakePickUp()
     {
         yield return new WaitForSeconds(0.2f);
         var currentTrans = transform;
-        Instantiate(sugarCube, currentTrans.position, currentTrans.rotation);
+        Instantiate(sugarCube, currentTrans.position, currentTrans.rotation).GetComponent<Rigidbody>().isKinematic =
+            false;
+
         Destroy(transform.gameObject);
         _coRef = null;
     }
-    
+
     private void TryAddOrDelete(GameObject obj)
     {
         try
         {
-            obj.GetComponent<IngredientContainer>().AddToContainer(iN); //WRITE CODE THAT CHECKS IF THIS INGREDIENT IS ALREADY ON LIST. IF SO ONLY USE THE AMOUNT AND DONT ADD THE ARRAY ELEMENT;
+            obj.GetComponent<IngredientContainer>()
+                .AddToContainer(
+                    iN); //WRITE CODE THAT CHECKS IF THIS INGREDIENT IS ALREADY ON LIST. IF SO ONLY USE THE AMOUNT AND DONT ADD THE ARRAY ELEMENT;
             Destroy(gameObject);
         }
         catch
@@ -46,4 +48,3 @@ public class SugarCube : MonoBehaviour
         }
     }
 }
-
