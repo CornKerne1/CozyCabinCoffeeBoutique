7c0e7121098105a0362d8bb0092a697e38964a8a
CornKernel <88573972+CornKerne1@users.noreply.github.com>
2022-08-30 21:02:39 -0700

    some pixel art character things

---
+++ OperationCoffeeShop/Assets/Programming/Matthew/LoopManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopManager : MonoBehaviour
+{
+[SerializeField] private List<GameObject> _playerPoints = new List<GameObject>();
+
+
+
+}
