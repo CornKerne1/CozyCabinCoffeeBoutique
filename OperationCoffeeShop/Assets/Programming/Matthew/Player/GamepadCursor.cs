35d454c32d335c6a3e5437702ccdcf870e81efba
CornKernel <88573972+CornKerne1@users.noreply.github.com>
2023-03-13 23:34:42 -0700

    More controller support

--- OperationCoffeeShop/Assets/Programming/Matthew/Player/GamepadCursor.cs
+++ OperationCoffeeShop/Assets/Programming/Matthew/Player/GamepadCursor.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.UI;
 
 public class GamepadCursor : MonoBehaviour
@@ -21,24 +20,21 @@ public class GamepadCursor : MonoBehaviour
     private float _screenHeight;
     private GameObject _currentButton;
 
-    private void Start()
+    private void Awake()
     {
-        if (playerInput.inputType.Contains("Mouse"))
-        {
-            Cursor.visible = true;
-        }
-        else
-        {
-            Cursor.visible = false;
-        }
-
-        Cursor.lockState = CursorLockMode.None;
         _virtualCursorRectTransform = cursorObject.GetComponent<RectTransform>();
         var sizeDelta = _virtualCursorRectTransform.sizeDelta;
         _cursorHalfWidth = sizeDelta.x / 2f;
         _cursorHalfHeight = sizeDelta.y / 2f;
         _screenWidth = Screen.width;
         _screenHeight = Screen.height;
+    }
+
+    private void Start()
+    {
+        Cursor.visible = !playerInput.inputType.Contains("Mouse");
+        Cursor.lockState = CursorLockMode.None;
+
         PlayerInput.InteractEvent += OnClickAction;
     }
 
@@ -51,77 +47,82 @@ public class GamepadCursor : MonoBehaviour
     }
 
     private void Update()
-{
-    if (!playerInput) return;
-
-    // Store the previous button for pointer exit
-    GameObject previousButton = _currentButton;
-
-    // Raycast to find the button under the cursor
-    PointerEventData eventData = new PointerEventData(EventSystem.current);
-    eventData.position = _virtualCursorRectTransform.position;
-    List<RaycastResult> results = new List<RaycastResult>();
-    EventSystem.current.RaycastAll(eventData, results);
-
-    // Find the button with the highest priority
-    _currentButton = null;
-    foreach (RaycastResult result in results)
     {
-        UnityEngine.UI.Button button = result.gameObject.GetComponent<UnityEngine.UI.Button>();
-        if (button != null)
+        if (!playerInput) return;
+
+        // Store the previous button for pointer exit
+        GameObject previousButton = _currentButton;
+
+        // Raycast to find the button under the cursor
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = _virtualCursorRectTransform.position;
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+
+        // Find the button with the highest priority
+        _currentButton = null;
+        int highestSortingOrder = int.MinValue;
+        foreach (RaycastResult result in results)
         {
-            if (_currentButton == null || result.sortingOrder > results[0].sortingOrder)
+            UnityEngine.UI.Button button = result.gameObject.GetComponent<UnityEngine.UI.Button>();
+            if (button != null && result.sortingOrder > highestSortingOrder)
             {
+                highestSortingOrder = result.sortingOrder;
                 _currentButton = result.gameObject;
             }
         }
-    }
 
-    // Call pointer enter and pointer exit handlers
-    if (previousButton != _currentButton)
-    {
-        if (previousButton != null)
+        // Call pointer enter and pointer exit handlers
+        if (previousButton != _currentButton)
         {
-            ExecuteEvents.Execute(previousButton, eventData, ExecuteEvents.pointerExitHandler);
+            if (previousButton != null)
+            {
+                ExecuteEvents.Execute(previousButton, eventData, ExecuteEvents.pointerExitHandler);
+            }
+
+            if (_currentButton != null)
+            {
+                ExecuteEvents.Execute(_currentButton, eventData, ExecuteEvents.pointerEnterHandler);
+            }
         }
 
-        if (_currentButton != null)
+        // Move the virtual cursor
+        if (playerInput.inputType.Contains("Stick"))
         {
-            ExecuteEvents.Execute(_currentButton, eventData, ExecuteEvents.pointerEnterHandler);
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+
+            _virtualCursorRectTransform.gameObject.SetActive(true);
+
+            Vector3 currentPosition = _virtualCursorRectTransform.position;
+            Vector3 targetPosition = currentPosition + new Vector3(playerInput.GetMouseX(), playerInput.GetMouseY(), 0) * (speed * playerInput.pD.mouseSensitivityOptions);
+            targetPosition.x = Mathf.Clamp(targetPosition.x, _cursorHalfWidth, _screenWidth - _cursorHalfWidth);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, _cursorHalfHeight, _screenHeight - _cursorHalfHeight);
+            _virtualCursorRectTransform.position = targetPosition;
+        }
+        else if (playerInput.inputType.Contains("Mouse"))
+        {
+            // Reset virtual cursor position to mouse position
+            _virtualCursorRectTransform.position = Mouse.current.position.ReadValue();
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            _virtualCursorRectTransform.gameObject.SetActive(false);
         }
     }
 
-    // Move the virtual cursor
-    if (playerInput.inputType.Contains("Stick"))
-    {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-        _virtualCursorRectTransform.gameObject.SetActive(true);
-        Vector3 currentPosition = _virtualCursorRectTransform.position;
-        Vector3 targetPosition = currentPosition +
-                                 new Vector3(playerInput.GetMouseX(), playerInput.GetMouseY(), 0) *
-                                 (speed * Time.deltaTime);
-        targetPosition.x = Mathf.Clamp(targetPosition.x, _cursorHalfWidth, _screenWidth - _cursorHalfWidth);
-        targetPosition.y = Mathf.Clamp(targetPosition.y, _cursorHalfHeight, _screenHeight - _cursorHalfHeight);
-        _virtualCursorRectTransform.position = targetPosition;
-    }
-    else if (playerInput.inputType.Contains("Mouse"))
-    {
-        // Reset virtual cursor position to mouse position
-        _virtualCursorRectTransform.position = Mouse.current.position.ReadValue();
-
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
-        _virtualCursorRectTransform.gameObject.SetActive(false);
-    }
-}
     private void OnClickAction(object sender, EventArgs e)
     {
         if (_currentButton != null)
         {
+            // Create a pointer event data with the current position of the virtual cursor
             PointerEventData eventData = new PointerEventData(EventSystem.current);
             eventData.position = _virtualCursorRectTransform.position;
+
+            // Execute a click event on the current button
             ExecuteEvents.Execute(_currentButton, eventData, ExecuteEvents.pointerClickHandler);
+
+            // Hide the system cursor and lock it to the center of the screen
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
         }
