f68f4e1b29ac1d29f39d72fd2abc348d2f213f57
CornKernel <88573972+CornKerne1@users.noreply.github.com>
2023-06-26 16:53:41 -0700

    objmovement

--- OperationCoffeeShop/Assets/Programming/Matthew/Player/PlayerInteraction.cs
+++ OperationCoffeeShop/Assets/Programming/Matthew/Player/PlayerInteraction.cs
@@ -223,7 +223,7 @@ public class PlayerInteraction : MonoBehaviour
     private void MoveObj(object sender, EventArgs e)
     {
         if (_rotate||playerData.inUI) return;
-        _carryDistance = Mathf.Clamp(_carryDistance + (playerInput.GetCurrentObjDistance() / 14f),
+        _carryDistance = Mathf.Clamp(_carryDistance + (playerInput.GetCurrentObjDistance() / 24f),
             playerData.carryDistance - playerData.carryDistanceClamp,
             playerData.carryDistance + playerData.carryDistanceClamp);
     }
